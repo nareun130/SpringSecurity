cffd96752a594fbaf491c849d39cba9856e1b379
DESKTOP-AJCLGM9\nareun130 <nareun130@gmail.com>
2024-11-13 14:30:12 +0900

    feat : 기본 폼 비활성화

--- easy-bank/src/main/java/com/nareun130/easy_bank/config/ProjectSecurityConfig.java
+++ easy-bank/src/main/java/com/nareun130/easy_bank/config/ProjectSecurityConfig.java
@@ -5,14 +5,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defauSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
-        http.formLogin(withDefaults());
+        // 모든 요청은 보호, 허가, 거부 => 권한 x : 403
+        // http.authorizeHttpRequests(requests ->
+        // requests.anyRequest().authenticated());
+        // http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll());
+        // http.authorizeHttpRequests(requests -> requests.anyRequest().denyAll());
+        http.authorizeHttpRequests(
+                requests -> requests.requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
+                        .requestMatchers("/notices", "/contact").permitAll());
+        // 기본 폼 로그인
         http.httpBasic(withDefaults());
+        //* 기본 폼 로그인 비활성화
+        // http.formLogin(flc -> flc.disable());//flc -> formLoginConfigurer
+        // http basic 활성화 : username, password를 base64로 인코딩하여 header에 추가
+        http.httpBasic(withDefaults());
+        // http.httpBasic(hbc->hbc.disable());//hbc -> httpBasicConfigurer;
         return http.build();
     }
 }
