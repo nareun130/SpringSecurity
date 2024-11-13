8655f6b67b337f2b9540aece8fa5ccdcd68202bd
DESKTOP-AJCLGM9\nareun130 <nareun130@gmail.com>
2024-11-13 14:44:06 +0900

    feat : InMemoryUserDetailsManager를 이용한 사용자 구성

--- easy-bank/src/main/java/com/nareun130/easy_bank/config/ProjectSecurityConfig.java
+++ easy-bank/src/main/java/com/nareun130/easy_bank/config/ProjectSecurityConfig.java
@@ -3,6 +3,10 @@ package com.nareun130.easy_bank.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -20,12 +24,19 @@ public class ProjectSecurityConfig {
                 requests -> requests.requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
                         .requestMatchers("/notices", "/contact").permitAll());
         // 기본 폼 로그인
-        http.httpBasic(withDefaults());
-        //* 기본 폼 로그인 비활성화
+        http.formLogin(withDefaults());
+        // * 기본 폼 로그인 비활성화
         // http.formLogin(flc -> flc.disable());//flc -> formLoginConfigurer
         // http basic 활성화 : username, password를 base64로 인코딩하여 header에 추가
         http.httpBasic(withDefaults());
         // http.httpBasic(hbc->hbc.disable());//hbc -> httpBasicConfigurer;
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}54321").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
