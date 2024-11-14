b1486a73491fc57f2cda29542346efd41487dbbe
DESKTOP-AJCLGM9\nareun130 <nareun130@gmail.com>
2024-11-14 16:42:14 +0900

    feat : 사용자 등록 메서드 추가

--- easy-bank/src/main/java/com/nareun130/easy_bank/config/ProjectSecurityConfig.java
+++ easy-bank/src/main/java/com/nareun130/easy_bank/config/ProjectSecurityConfig.java
@@ -25,9 +25,12 @@ public class ProjectSecurityConfig {
         // requests.anyRequest().authenticated());
         // http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll());
         // http.authorizeHttpRequests(requests -> requests.anyRequest().denyAll());
-        http.authorizeHttpRequests(
-                requests -> requests.requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
-                        .requestMatchers("/notices", "/contact").permitAll());
+        //* csrf 비활성화 : 기본적으로 POST,PUT,DELETE에 대해 Security가 보호 
+        http.csrf(csrfConfig -> csrfConfig.disable())
+                .authorizeHttpRequests(
+                        requests -> requests.requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans")
+                                .authenticated()
+                                .requestMatchers("/notices", "/contact", "/register").permitAll());
         // 기본 폼 로그인
         http.formLogin(withDefaults());
         // * 기본 폼 로그인 비활성화
@@ -38,23 +41,24 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
-    //* 커스텀 UserDetailsService를 구현 하였기에 필요 x
+    // * 커스텀 UserDetailsService를 구현 하였기에 필요 x
     // @Bean
     // public UserDetailsService userDetailsService(DataSource dataSource) {
-    //     return new JdbcUserDetailsManager(dataSource);
+    // return new JdbcUserDetailsManager(dataSource);
     // }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         // return new BCryptPasswordEncoder();
-        // * 기본적으로 BCryptPasswordEncoder 이용
+        // * 기본적으로 BCryptPasswordEncoder 이용 
+        //! BcryptPasswordEncoder를 직접 사용 x -> DelegatingPasswordEncoder를 사용할 것! 표준이 바뀌었을 때를 대비
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    //spring security 6.3부터 가능
-    //* CompromisedPasswordChecker : 비밀번호가 데이터 유출 사고에 노출된 적 있는지 확인하는 기능
+    // spring security 6.3부터 가능
+    // * CompromisedPasswordChecker : 비밀번호가 데이터 유출 사고에 노출된 적 있는지 확인하는 기능
     @Bean
     public CompromisedPasswordChecker compromisedPasswordChecker() {
-        return new HaveIBeenPwnedRestApiPasswordChecker();//~> 실패 시 403 에러
+        return new HaveIBeenPwnedRestApiPasswordChecker();// ~> 실패 시 403 에러
     }
 }
