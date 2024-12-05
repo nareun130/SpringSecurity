46dcf302365828b907f795e849442185f9ded333
DESKTOP-AJCLGM9\nareun130 <nareun130@gmail.com>
2024-12-05 14:00:12 +0900

    feat : 메소드 레벨 보안 적용

--- eazybank/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ eazybank/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -2,10 +2,12 @@ package com.eazybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableWebSecurity(debug = false) //* 운영 사용 x 
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class EazyBankBackendApplication {
 
     public static void main(String[] args) {
--- eazybank/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ eazybank/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -85,7 +85,8 @@ public class ProjectSecurityConfig {
                         // .requestMatchers("/myCards").hasAuthority("VIEWCARDS")
                         .requestMatchers("/myAccount").hasRole("USER")
                         .requestMatchers("/myBalance").hasAnyRole("USER", "ADMIN")
-                        .requestMatchers("/myLoans").hasRole("USER")
+                        // .requestMatchers("/myLoans").hasRole("USER")
+                        .requestMatchers("/myLoans").authenticated()
                         .requestMatchers("/myCards").hasRole("USER")
                         .requestMatchers("/user").authenticated()
                         .requestMatchers("/notices", "/contact", "/error", "/register", "/invalidSession","/apiLogin").permitAll());
