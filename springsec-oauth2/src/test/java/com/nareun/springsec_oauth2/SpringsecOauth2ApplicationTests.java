dd38ae379f588b344b7e8c3ca8fdd67fc7f3f7ef
DESKTOP-AJCLGM9\nareun130 <nareun130@gmail.com>
2024-12-05 14:00:34 +0900

    feat : oauth2 프로젝트 생성

---
+++ springsec-oauth2/src/test/java/com/nareun/springsec_oauth2/SpringsecOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nareun.springsec_oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
